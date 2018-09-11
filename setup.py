752cc50eee8c6b72ea482da5881b0f99398e5020
Paradoxis <luke@paradoxis.nl>
2018-09-11 20:18:48 +0200

    Updated manifest, gitignore and setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,116 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import os
+import re
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
 
 
+# Package meta-data.
+NAME = 'transip_stack'
+DESCRIPTION = 'Unofficial wrapper for the TransIP STACK API'
+URL = 'https://github.com/Paradoxis/TransIP-STACK-API'
+EMAIL = 'luke@paradoxis.nl'
+AUTHOR = 'Luke Paris (Paradoxis)'
+REQUIRES_PYTHON = '>=3.6.0'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'bs4>=0.0.1',
+    'lxml>=3.8.0',
+    'requests>=2.18.2',
+    'webdavclient>=1.0.8',
+    'pycurl==7.43.0'
+]
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = ('\n' + f.read()).strip()
+
+with io.open(os.path.join(here, NAME, '__init__.py'), encoding='utf-8') as init:
+    VERSION = re.search(r'__version__ = [\'"]([\d.]+)[\'"]', init.read()).group(1)
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name='transip-stack-api',
-    version='1.0.2',
-    install_requires=[
-        'bs4>=0.0.1',
-        'lxml>=3.8.0',
-        'requests>=2.18.2',
-        'webdavclient>=1.0.8',
-        'pycurl==7.43.0'
-    ],
-    url='https://github.com/Paradoxis/TransIP-STACK-API',
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    include_package_data=True,
     license='MIT',
-    author='Paradoxis',
-    author_email='luke@paradoxis.nl',
-    description='Unofficial wrapper for the TransIP STACK API',
-    keywords=['TransIP', 'STACK', 'Storage'],
-    download_url='https://codeload.github.com/Paradoxis/TransIP-STACK-API/tar.gz/master')
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
--- transip_stack/__init__.py
+++ transip_stack/__init__.py
@@ -1,2 +1,3 @@
+__version__ = '1.0.3'
 from transip_stack.stack import Stack
 from transip_stack.exceptions import StackException
