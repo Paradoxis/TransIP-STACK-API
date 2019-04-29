6640952a5226e072ba6ecaa961fca7f8bfacbe1c
Paradoxis <luke@paradoxis.nl>
2019-04-29 22:27:46 +0200

    Fixed TypeError: object of type 'NoneType' has no len()

--- transip_stack/stack.py
+++ transip_stack/stack.py
@@ -172,7 +172,7 @@ class Stack:
 
         while amount is None or (amount is not None and offset < int(amount)):
             files = self.__files(path, offset, query=search, order=order)
-            offset += len(files["nodes"])
+            offset += len(files.get("nodes") or [])
             amount = int(files.get("amount"))
             yield from self.__nodes_to_objects(files.get("nodes") or [])
 
